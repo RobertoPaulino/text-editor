eadd9c727e4c86f09541388a11c35c5ee1d09f75
Robert <rpaulinodev@gmail.com>
2024-12-15 22:25:05 -0400

    Removes signal flags, preventing unwanted behavior for Ctrl-c/v/y.

--- kilo.c
+++ kilo.c
@@ -23,7 +23,7 @@ void enableRawMode() {
 
   //Take bitwise-NOT of ECHO and perform bitwise-AND on the local
   //flags to disable ECHO.
-  raw.c_lflag &= ~(ECHO | ICANON);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
   
   //set the flags
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
