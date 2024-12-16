a4855cb6f223906ee9c763abed13932284e18e4b
Robert <rpaulinodev@gmail.com>
2024-12-15 21:13:43 -0400

    Resets flags for terminal attribute on exit.

--- kilo.c
+++ kilo.c
@@ -1,12 +1,24 @@
 #include <unistd.h>
 #include <termios.h>
+#include <stdlib.h>
+
+struct termios orig_termios;
+
+void disableRawMode() {
+  //after the editor is done we reset the attributes.
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
 
 void enableRawMode() {
-  struct termios raw;
+
+  //get terminal attributes.
+  tcgetattr(STDIN_FILENO, &orig_termios);
   
-  //get terminal attributes
-  tcgetattr(STDIN_FILENO, &raw);
-  
+  //reset attributes when exit is proccessed.
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
+
   //Take bitwise-NOT of ECHO and perform bitwise-AND on the local
   //flags to disable ECHO.
   raw.c_lflag &= ~(ECHO);
